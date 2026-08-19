bf5281812c399434ec8eefb75617b0ad3cfae0f8
Gustavo <ggonzalezcarrasco18@gmail.com>
2026-08-18 22:17:12 -0400

    Fix: Inyectar credenciales Web de Firebase

--- frontend/meal_ia/build/web/flutter_service_worker.js
+++ frontend/meal_ia/build/web/flutter_service_worker.js
@@ -37,7 +37,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "0d25009df69330232a8376b861e229eb
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "392db06183525ce7fe40e6a0d3b9545d",
+"flutter_bootstrap.js": "a02822b689ed3de7aa49b02607cdfb9c",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
